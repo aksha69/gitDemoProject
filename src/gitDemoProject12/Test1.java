80ce80714a904fc54a203ac6daa759c7d1236c8a
akshay mude <akshay mude@LAPTOP-8ROU7R0R>
2022-08-28 00:04:29 +0530

    First Commit....

---
+++ src/gitDemoProject12/Test1.java
@@ -0,0 +1,13 @@
+package gitDemoProject12;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("this is new java program");
+		System.out.println("we are learning java program");
+		System.out.println("my name is akshay");
+
+
+	}
+
+}
